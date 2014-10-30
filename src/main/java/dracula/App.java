964e73f20fbf02b9beaf93b9c3dc15999822ae64
Unnikrishnan <chathotu@UKLON-07995.local>
2014-10-30 17:40:30 +0000

    seconnd commit

---
+++ src/main/java/dracula/App.java
@@ -0,0 +1,13 @@
+package dracula;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
